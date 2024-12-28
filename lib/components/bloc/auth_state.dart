fa7ffeaaafdbb1924b9fd3422a5b407bacad0272
Iskander2323 <iska200314@gmail.com>
2024-12-28 23:15:22 +0500

    changed auth state status

--- lib/components/bloc/auth_state.dart
+++ lib/components/bloc/auth_state.dart
@@ -1,28 +1,31 @@
 part of 'auth_bloc.dart';
 
-enum AuthStateStatus { initial, success, loading, error }
+enum AuthStateStatus { initial, register, success, loading }
 
 final class AuthState extends Equatable {
   const AuthState(
       {this.status = AuthStateStatus.initial,
       this.userCredential,
-      this.errorMessage});
+      this.errorMessage,
+      this.isError});
 
   final AuthStateStatus status;
   final UserCredential? userCredential;
   final String? errorMessage;
+  final bool? isError;
 
   AuthState copyWith(
       {AuthStateStatus? status,
       UserCredential? userCredential,
-      String? errorMessage}) {
+      String? errorMessage,
+      bool? isError}) {
     return AuthState(
-      status: status ?? this.status,
-      userCredential: userCredential ?? this.userCredential,
-      errorMessage: errorMessage ?? this.errorMessage,
-    );
+        status: status ?? this.status,
+        userCredential: userCredential ?? this.userCredential,
+        errorMessage: errorMessage ?? this.errorMessage,
+        isError: isError ?? this.isError);
   }
 
   @override
-  List<Object?> get props => [status, userCredential, errorMessage];
+  List<Object?> get props => [status, userCredential, errorMessage, isError];
 }
