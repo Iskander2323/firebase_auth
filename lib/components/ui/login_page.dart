7c9233fe02638a2aed25bcf06db151a01ad39481
Iskander2323 <iska200314@gmail.com>
2024-12-28 23:14:12 +0500

    added functional of login

--- lib/components/ui/login_page.dart
+++ lib/components/ui/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth_app/components/bloc/auth_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,8 +10,101 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailTextController = TextEditingController();
+  final TextEditingController _passwordTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Login Page',
+                style: TextStyle(fontSize: 30),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                controller: _emailTextController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Email field cannot be empty';
+                  }
+                  final emailRegExp = RegExp(
+                    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+                  );
+                  if (!emailRegExp.hasMatch(value)) {
+                    return 'Enter a valid email';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 15),
+              TextFormField(
+                controller: _passwordTextController,
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Password field cannot be empty';
+                  }
+                  if (value.length < 6) {
+                    return 'Password must be at least 6 characters';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextButton(
+                onPressed: () {
+                  context.read<AuthBloc>().add(RegisterNewAccountEvent());
+                },
+                child: const Text('Or register new account'),
+              ),
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  if (_formKey.currentState!.validate()) {
+                    context.read<AuthBloc>().add(
+                          LoginWithEmailEvent(
+                            email: _emailTextController.text,
+                            password: _passwordTextController.text,
+                          ),
+                        );
+                  }
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
   }
 }
